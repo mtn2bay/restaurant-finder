b4da370d7cd7cb95586fd4066bc19bed663d8a1c
mtn2bay <mikemroy@gmail.com>
2017-06-10 22:07:14 -0400

    Google places, search functionality, adjust fonts

--- js/app.js
+++ js/app.js
@@ -1,7 +1,66 @@
+
+var searchBox = document.getElementById("search");
+var map, service, infoWindow, userQuery, activateMarkers;
+
 function initMap() {
-  var loc = {lat: 43.657134, lng: -70.259530};
-  var map = new google.maps.Map(document.getElementById('map-container'), {
-    zoom: 14,
-    center: loc
+  var center = new google.maps.LatLng(43.656758, -70.256169);
+
+  map = new google.maps.Map(document.getElementById('map-container'), {
+    center: center,
+    zoom: 17,
+    disableDefaultUI: true
+  });
+
+  var request = {
+    location: center,
+    radius: 8000,
+    query: userQuery,
+    types: ['restaurant']
+  };
+
+  infoWindow = new google.maps.InfoWindow();
+
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i]
+      if (activateMarkers) {
+        createMarker(results[i]);
+      }
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeloc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(place.name);
+    infoWindow.open(map, this);
   });
 }
+
+searchBox.addEventListener('focus', function(event) {
+  event.target.setAttribute('placeholder', '');
+});
+searchBox.addEventListener('blur', function(event) {
+  event.target.setAttribute('placeholder', 'e.g. Taqueria');
+});
+
+searchBox.addEventListener('keyup', function(event) {
+  event.preventDefault();
+  if (event.keyCode == 13) {
+    userQuery = this.value;
+    this.value = '';
+    initMap();
+    activateMarkers = true;
+  }
+});
